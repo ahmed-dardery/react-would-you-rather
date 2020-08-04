9a304c894a8f090fb0ea0990b8a4edf202e5aff4
Ahmed Dardery <megadardery@yahoo.com>
2020-08-04 03:18:40 +0200

    feat: Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {setAuthedUser} from "../actions/authedUser";
+import {Redirect} from "react-router-dom";
 
-class Login extends Component{
-    render(){
+class Login extends Component {
+    state = {
+        selected: 'none',
+        redirect: false
+    };
+
+    login_click = (e) => {
+        e.preventDefault();
+
+        const {selected} = this.state;
+        this.props.dispatch(setAuthedUser(selected));
+
+        this.setState({redirect: true});
+    };
+
+    select_change = (e) => {
+        this.setState({selected: e.target.value});
+    };
+
+    render() {
+        const {selected, redirect} = this.state;
+        if (redirect) return <Redirect to='/'/>;
+
+        const {users} = this.props;
         return (
-            <div>
-                Login
-            </div>
+            <form onSubmit={this.login_click}>
+                Login as:
+                <select value={selected} onChange={this.select_change}>
+                    <option value="none" disabled>Select a user</option>
+                    {
+                        Object.keys(users).map(user =>
+                            <option key={user} value={user}>{users[user].name}</option>
+                        )
+                    }
+                </select>
+                <button disabled={selected === 'none'} type="submit">Login</button>
+            </form>
         )
     }
 }
 
-export default Login;
+export default connect(({users}) => ({users}))(Login);
