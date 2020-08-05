66865152b657cd07bb87ac845ecfe2213515632c
Ahmed Dardery <megadardery@yahoo.com>
2020-08-05 02:08:37 +0200

    feat: question answer indicator

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,17 +5,19 @@ import {findPick} from "../utils/helper";
 import Block from "./Block";
 import {handleAnswerQuestion} from "../actions/questions";
 
-const ProgressBar = ({option, total}) => {
+const ProgressBar = ({option, total, highlight}) => {
     const votes = option.votes.length;
     const p = (votes * 100 / total).toFixed(1);
     const percent = `${p}%`;
 
     return (
-        <div>
+        <div className="progress-box">
+            {highlight && <div className='progress-corner'>✓</div>}
             {option.text}
             <div className="progress-border">
                 {!(p >= 10) && <div style={{width: '100%', position: 'absolute'}}>{percent}</div>}
-                <div style={{width: percent}} className="progress-fill">{(p >= 10) && percent}</div>
+                <div style={{width: percent}}
+                     className={`progress-fill ${highlight && 'highlight'}`}>{(p >= 10) && percent}</div>
             </div>
             <div className="progress-text">{`${votes} out of ${total} votes`}</div>
         </div>
@@ -46,7 +48,7 @@ class Question extends Component {
 
         const {authedUser} = this.props;
         const qid = this.props.question.id;
-        const answer = this.state.selected ? 'optionOne' : 'optionTwo';
+        const answer = 'option' + this.state.selected;
         this.props.dispatch(handleAnswerQuestion({authedUser, qid, answer}))
     };
 
@@ -74,9 +76,9 @@ class Question extends Component {
         const questionPoll = () => (
             <Fragment>
                 <h3>Would you rather</h3>
-                <ProgressBar option={question.optionOne} total={total}/>
+                <ProgressBar highlight={choice === 1} option={question.optionOne} total={total}/>
                 <p className='block-content-center'>Or</p>
-                <ProgressBar option={question.optionTwo} total={total}/>
+                <ProgressBar highlight={choice === 2} option={question.optionTwo} total={total}/>
             </Fragment>
         );
 
