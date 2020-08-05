98354ec7a9d86995b6f63ff29b94d2d7567b6f89
Ahmed Dardery <megadardery@yahoo.com>
2020-08-05 02:20:22 +0200

    Updates

---
+++ precache-manifest.f2fa12a7e30c57965a196f99d76d5905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a6163f6359d62327f6538b8e911445",
+    "url": "/react-would-you-rather/index.html"
+  },
+  {
+    "revision": "9b4f7b39a7e3f490a3ab",
+    "url": "/react-would-you-rather/static/css/main.9253291d.chunk.css"
+  },
+  {
+    "revision": "1f5c340d21316a5bde7f",
+    "url": "/react-would-you-rather/static/js/2.6e28e062.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-would-you-rather/static/js/2.6e28e062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4f7b39a7e3f490a3ab",
+    "url": "/react-would-you-rather/static/js/main.0b2aadb9.chunk.js"
+  },
+  {
+    "revision": "7cba3667499318f503fc",
+    "url": "/react-would-you-rather/static/js/runtime-main.302a6eda.js"
+  }
+]);
