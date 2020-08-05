fe22d7dc04899a23d8e5b59f34a7dfd1eb7e4c40
Ahmed Dardery <megadardery@yahoo.com>
2020-08-05 21:53:16 +0200

    feat: 404 page design improvement

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,8 +3,11 @@ import React, {Component} from 'react';
 class NotFound extends Component{
     render(){
         return (
-            <div>
-                404 Not Found
+            <div className="window">
+                <div className="error-404">404</div>
+                <div className="error-note">That's sad!</div>
+                <div className="error-msg">The page you are looking for doesn't exist, or is unavailable</div>
+                {/*<button className="block-content-button">Go to Homepage</button>*/}
             </div>
         )
     }
