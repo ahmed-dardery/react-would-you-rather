a1888182e747dd03c1c1b858189d96e28d62a91e
Ahmed Dardery <megadardery@yahoo.com>
2020-08-05 02:28:10 +0200

    feat: redesign login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {setAuthedUser} from "../actions/authedUser";
 import {Redirect} from "react-router-dom";
+import Block from "./Block";
 
 class Login extends Component {
     state = {
@@ -26,18 +27,23 @@ class Login extends Component {
 
         const {users} = this.props;
         return (
-            <form onSubmit={this.login_click}>
-                Login as:
-                <select value={selected} onChange={this.select_change}>
-                    <option value="none" disabled>Select a user</option>
-                    {
-                        Object.keys(users).map(user =>
-                            <option key={user} value={user}>{users[user].name}</option>
-                        )
-                    }
-                </select>
-                <button disabled={selected === 'none'} type="submit">Login</button>
-            </form>
+            <div className="window">
+                <Block title="Login" centerTitle innerContent={
+                    <form onSubmit={this.login_click}>
+                        <h3>Login as:</h3>
+                        <select className="block-input" value={selected} onChange={this.select_change}>
+                            <option value="none" disabled>Select a user</option>
+                            {
+                                Object.keys(users).map(user =>
+                                    <option key={user} value={user}>{users[user].name}</option>
+                                )
+                            }
+                        </select>
+                        <button className='block-content-button' disabled={selected === 'none'} type="submit">Login</button>
+                    </form>
+                }/>
+            </div>
+
         )
     }
 }
